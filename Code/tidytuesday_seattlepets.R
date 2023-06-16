4095b4ded03e386c9188bc7dd4b427c3d1486744
Kaizad Patel <50244730+kaizadp@users.noreply.github.com>
2023-06-16 14:33:39 -0700

    summarise and graphs

--- Code/tidytuesday_seattlepets.R
+++ Code/tidytuesday_seattlepets.R
@@ -1,9 +1,29 @@
 # load packages -----------------------------------------------------------
 
-library(ggplot2)
-
+library(tidyverse)
 
 # seattle pets data -------------------------------------------------------
 
 seattle_pets <- readr::read_csv("https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2019/2019-03-26/seattle_pets.csv")
 
+pets_summary <- 
+  seattle_pets %>% 
+  mutate(primary_breed = str_replace(primary_breed, "-", " ")) %>% 
+  group_by(species, primary_breed) %>% 
+  summarize(breed_counts = n())
+  #mutate(primary_breed = str_remove(primary_breed, "-")) %>% 
+
+pets_summary %>% 
+  ggplot(aes(x = primary_breed, y = breed_counts))+
+  geom_point()+
+  facet_wrap(~species, scales = "free_x")
+
+pets_summary_greater_500 <- 
+  pets_summary %>% 
+  filter(breed_counts > 500)
+
+pets_summary_greater_500 %>% 
+  ggplot(aes(x = primary_breed, y = breed_counts))+
+  geom_point()+
+  facet_wrap(~species, scales = "free_x")+
+  theme(axis.text.x = element_text(angle = 45))
