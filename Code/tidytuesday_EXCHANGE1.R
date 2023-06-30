8e71df85d2ee988450f0bf4b7a527956b936b304
dendrobatesauratus <sarahh_999@yahoo.com>
2023-06-30 13:16:34 -0700

    Update tidytuesday_EXCHANGE1.R

--- Code/tidytuesday_EXCHANGE1.R
+++ Code/tidytuesday_EXCHANGE1.R
@@ -1,4 +1,4 @@
-# imported data from files using Import Dataset
+# imported data from files using Import Dataset>From Text(base)
 
 # soil carbon weight percent by transect, jitter plot:
 ec1_soil_tc_L2 %>%
@@ -12,7 +12,8 @@ soil_carbon2 <-
   ec1_soil_tc_L2 %>%
   mutate(transect_location = factor(transect_location, levels = c("upland", "transition", "wetland")))
 
-# using the reordered transects, make boxplot of carbon weight percent by transect,
+# using the reordered transects, make boxplot (with jitter) of
+# carbon weight percent by transect,
 # filled by each transect (using set3 palette):
 soil_carbon2 %>%
   ggplot(aes(x = transect_location, y = carbon_weight_perc, fill = transect_location))+ 
@@ -20,19 +21,51 @@ soil_carbon2 %>%
   geom_jitter(width=0.2, alpha = 0.3)+
   labs(x = "Transect Location", y = "Carbon Weight (%)", title = "Percent of Carbon in Each Transect")+
   theme(plot.title = element_text(hjust = 0.5))+
-  scale_fill_brewer(palette="Set3")
+  scale_fill_brewer(palette="Set3")+
+  guides(fill = guide_legend(title = "Transect Location"))
 
-#average carbon weight percent for each transect location:
+# average carbon weight percent for each transect location:
 soil_carbon2 %>%
   group_by(transect_location) %>%
   dplyr::summarise(carbon_percent_mean = mean(carbon_weight_perc), n = n())
 
-#median carbon weight percent for each transect location:
+# median carbon weight percent for each transect location:
 soil_carbon2 %>%
   group_by(transect_location) %>%
   dplyr::summarise(carbon_percent_median = median(carbon_weight_perc), n = n())
 
-#standard deviation of carbon weight percent for each transect location:
+# standard deviation of carbon weight percent for each transect location:
 soil_carbon2 %>%
   group_by(transect_location) %>%
   dplyr::summarise(carbon_percent_sd = sd(carbon_weight_perc), n = n())
+
+# is there a way to run all three summaries at once?
+soil_carbon2 %>%
+  group_by(transect_location) %>%
+  dplyr::summarise(carbon_perc_mean = mean(carbon_weight_perc), n = n()) %>%
+  dplyr::summarise(carbon_perc_median = median(carbon_weight_perc), n = n()) %>%
+  dplyr::summarise(carbon_perc_sd = sd(carbon_weight_perc), n = n())
+
+# merge two datasets into one:
+merged <- merge(ec1_metadata_kitlevel, soil_carbon2)
+
+# from the merged datasets, create a boxplot showing carbon weight percent by region:
+merged %>%
+  ggplot(aes(x = region, y = carbon_weight_perc, fill = region))+ 
+  geom_boxplot()+
+  geom_jitter(width=0.2, alpha = 0.3)+
+  labs(x = "Region", y = "Carbon Weight (%)", title = "Percent of Carbon in Each Region")+
+  theme(plot.title = element_text(hjust = 0.5))+
+  scale_fill_brewer(palette="Accent")+
+  guides(fill = guide_legend(title = "Region"))
+
+# tried ANOVA:
+
+a = aov(carbon_weight_perc ~ transect_location, data = merged)
+summary(a)
+
+b = aov(carbon_weight_perc ~ region, data = merged)
+summary(b)
+
+ab = aov(carbon_weight_perc ~ transect_location + region, data = merged)
+summary(ab)
