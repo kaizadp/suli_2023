c4fc83246eba53444f8bb661c8375ab60765e755
dendrobatesauratus <sarahh_999@yahoo.com>
2023-07-24 15:36:56 -0700

    need help with the group_by
    I tried using the group_by in the aes like you said, it didn't smooth the lines out. Not sure what I'm doing wrong.

--- Code/2wk-water-optodes.R
+++ Code/2wk-water-optodes.R
@@ -7,7 +7,36 @@ optodes_data = read.csv("data/anoxia-redox/optode_processed_water_2wk_first24hr.
 
 optode_processed_water_2wk_first24hr %>% 
   ggplot(aes(x = time_minutes/60, y = do_mg_L))+
-  geom_point()+
+  geom_line(size = 0.5)+
   facet_wrap(~location)+
-  xlim(0, 2)+
+  
   labs(x = "Elapsed time, Hours", y = "Dissolved oxygen, mg/L")
+
+transition_a <-
+  optode_processed_water_2wk_first24hr %>%
+  subset(location == "transition-A")
+
+# how to smooth the lines by using group_by?
+transition_a %>%
+  ggplot(aes(x = time_minutes/60, y = do_mg_L, group_by(sample_label)))+
+  geom_line()+
+  xlim(0,2)+
+  labs(x = "Elapsed time, Hours", y = "Dissolved oxygen, mg/L", title = "Transition")
+
+upland_a <-
+  optode_processed_water_2wk_first24hr %>%
+  subset(location == "upland-A")
+
+upland_a %>%
+  ggplot(aes(x = time_minutes/60, y = do_mg_L))+
+  geom_line()+
+  labs(x = "Elapsed time, Hours", y = "Dissolved oxygen, mg/L", title = "Upland-A")
+
+upland_b <-
+  optode_processed_water_2wk_first24hr %>%
+  subset(location == "upland-B")
+
+upland_b %>%
+  ggplot(aes(x = time_minutes/60, y = do_mg_L))+
+  geom_line()+
+  labs(x = "Elapsed time, Hours", y = "Dissolved oxygen, mg/L", title = "Upland-B")
