5eb1505ae9e0e260fd7b543c4fbd33c0b1cf5a36
dendrobatesauratus <sarahh_999@yahoo.com>
2023-07-28 10:21:06 -0700

    updated data

--- Code/2wk-water-optodes.R
+++ Code/2wk-water-optodes.R
@@ -1,11 +1,10 @@
 library(tidyverse)
 
-#
 
-# load data ---------------------------------------------------------------
-optodes_data = read.csv("data/anoxia-redox/optode_processed_water_2wk_first24hr.csv")
 
-optodes_data %>% 
+# load data through "import dataset" ---------------------------------------------------------------
+
+optode_processed_water_2wk_first24hr %>% 
   ggplot(aes(x = time_minutes/60, y = do_mg_L, group = sample_label))+
   geom_line(size = 0.5)+
   facet_wrap(~location)+
@@ -13,30 +12,30 @@ optodes_data %>%
   labs(x = "Elapsed time, Hours", y = "Dissolved oxygen, mg/L")
 
 transition_a <-
-  optodes_data %>%
+  optode_processed_water_2wk_first24hr %>%
   subset(location == "transition-A")
 
 # how to smooth the lines by using group_by?
 transition_a %>%
-  ggplot(aes(x = time_minutes/60, y = do_mg_L, group_by(sample_label)))+
+  ggplot(aes(x = time_minutes/60, y = do_mg_L, group = sample_label))+
   geom_line()+
   xlim(0,2)+
   labs(x = "Elapsed time, Hours", y = "Dissolved oxygen, mg/L", title = "Transition")
 
 upland_a <-
-  optodes_data %>%
+  optode_processed_water_2wk_first24hr %>%
   subset(location == "upland-A")
 
 upland_a %>%
-  ggplot(aes(x = time_minutes/60, y = do_mg_L))+
+  ggplot(aes(x = time_minutes/60, y = do_mg_L, group = sample_label))+
   geom_line()+
   labs(x = "Elapsed time, Hours", y = "Dissolved oxygen, mg/L", title = "Upland-A")
 
 upland_b <-
-  optodes_data %>%
+  optode_processed_water_2wk_first24hr %>%
   subset(location == "upland-B")
 
 upland_b %>%
-  ggplot(aes(x = time_minutes/60, y = do_mg_L))+
+  ggplot(aes(x = time_minutes/60, y = do_mg_L, group = sample_label))+
   geom_line()+
   labs(x = "Elapsed time, Hours", y = "Dissolved oxygen, mg/L", title = "Upland-B")
